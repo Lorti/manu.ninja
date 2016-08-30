c5663363907ba7b165f61f70ae4fa53672320c2d
Manuel Wieser <office@manuelwieser.com>
2016-08-30 18:52:33 +0200

    dates and reading time

---
+++ _plugins/pluralize-filter.rb
@@ -0,0 +1,14 @@
+module PluralizeFilter
+	def pluralize(number, singular, plural = nil)
+		number = number.to_i
+		if number == 1
+			"#{number} #{singular}"
+		elsif plural.nil?
+			"#{number} #{singular}s"
+		else
+			"#{number} #{plural}"
+		end
+	end
+end
+
+Liquid::Template.register_filter(PluralizeFilter)
