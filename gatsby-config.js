534163e00cad2c2b33ac7d76cb51c75763abd134
Manuel Wieser <office@manuelwieser.com>
2017-08-31 21:48:25 +0200

    Feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,60 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
-      resolve: 'gatsby-plugin-feed',
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+                filter: {
+                  frontmatter: {
+                    draft: { ne: true }
+                  }
+                }
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    frontmatter {
+                      title
+                      date
+                      path
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: '/feed.xml'
+          }
+        ]
+      }
     },
     {
       resolve: 'gatsby-plugin-google-analytics',
