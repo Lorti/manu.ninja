09801ce7b4e777f0e21667ce61d50e006df04097
Manuel Wieser <office@manuelwieser.com>
2017-10-06 08:13:25 +0200

    Introduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,32 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
+import Introduction from '../components/Introduction'
+
 import excerpt from '../utils/excerpt'
 
 export default function Index({ data }) {
   const { edges: posts } = data.allMarkdownRemark
   return (
-    <div className="Column">
-      {posts
-        .filter(post => post.node.frontmatter.title.length > 0)
-        .map(({ node: post }) => {
-          return (
-            <div className="blog-post-preview" key={post.id}>
-              <h1>
-                <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
-              </h1>
-              <h2>{post.frontmatter.date}</h2>
-              <p>{excerpt(post.html)}</p>
-            </div>
-          )
-        })}
+    <div>
+      <Introduction />
+      <div className="Column">
+        {posts
+          .filter(post => post.node.frontmatter.title.length > 0)
+          .map(({ node: post }) => {
+            return (
+              <div className="blog-post-preview" key={post.id}>
+                <h1>
+                  <Link to={post.frontmatter.path}>
+                    {post.frontmatter.title}
+                  </Link>
+                </h1>
+                <h2>{post.frontmatter.date}</h2>
+                <p>{excerpt(post.html)}</p>
+              </div>
+            )
+          })}
+      </div>
     </div>
   )
 }
