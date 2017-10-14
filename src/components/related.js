bcbe059a1bca0743eac9162a3f467569da86fa24
Manuel Wieser <office@manuelwieser.com>
2017-10-14 15:25:53 +0200

    Fix external links in related articles component

---
+++ src/components/related.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import mapTag from '../utils/taxonomy.js'
+
+function RelatedLink({ title, path, external }) {
+  if (external) {
+    return (
+      <a className="Links-link u-truncated" href={external} target="_blank">
+        {title}
+      </a>
+    )
+  }
+  return (
+    <Link className="Links-link u-truncated" to={path}>
+      {title}
+    </Link>
+  )
+}
+
+export default function Related({ posts = [] }) {
+  return (
+    <div className="Links">
+      <h3 className="Links-heading">You may also like…</h3>
+      <ul>
+        {posts.map(post => {
+          return (
+            <li key={post.node.frontmatter.path} className="Links-item">
+              <span className="Links-meta">
+                {post.node.frontmatter.categories.map(category => {
+                  return (
+                    <Link key={category} to={`/categories/${category}`}>
+                      {mapTag(category)}
+                    </Link>
+                  )
+                })}
+              </span>
+              <RelatedLink
+                title={post.node.frontmatter.title}
+                path={post.node.frontmatter.path}
+                external={post.node.frontmatter.external}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
