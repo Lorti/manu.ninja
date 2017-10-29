fae5f5d8eca9a0a949e447d9767ff9bc609d3873
Manuel Wieser <office@manuelwieser.com>
2017-10-29 11:49:43 +0100

    Repair JSON-LD in client-side and server-side rendering

--- src/components/schema/article.js
+++ src/components/schema/article.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
 export default function Article({ title, description, date, imageUrl }) {
+  const data = `{
+    "@context": "http://schema.org/",
+    "@type": "BlogPosting",
+    "author": {
+      "@type": "Person",
+      "name": "Manuel Wieser"
+    },
+    "headline": "${title}",
+    "datePublished": "${new Date(date).toISOString()}",
+    "image": "${imageUrl}",
+    "description": "${description}"
+  }`
   return (
-    <script type="application/ld+json">
-      {JSON.stringify({
-        '@context': 'http://schema.org/',
-        '@type': 'BlogPosting',
-        author: {
-          '@type': 'Person',
-          name: 'Manuel Wieser',
-        },
-        headline: title,
-        datePublished: new Date(date).toISOString(),
-        image: imageUrl,
-        description: description,
-      })}
-    </script>
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: data }}
+    />
   )
 }
