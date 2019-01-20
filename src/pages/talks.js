804c1e6c28c4f9bf00089cf91960a0a146223d7b
Manuel Wieser <office@manuelwieser.com>
2019-01-20 18:28:49 +0100

    Add a list of talks

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -7,19 +7,56 @@ export default function Talks({ data }) {
   return (
     <Layout>
       <div className="Column">
-        {projects.map(({ node: talk }) => {
-          return (
-            <div>
-              <p>
-                {talk.date} – {talk.location}
-              </p>
-              <h2>
-                <a href={talk.slides}>{talk.title}</a>
-              </h2>
-              <p>{talk.location}</p>
-            </div>
-          )
-        })}
+        <div className="Article">
+          <div className="Article-header u-textCenter">
+            <h1 className="Article-title">Talks</h1>
+          </div>
+          <div className="Article-content">
+            <img
+              style={{
+                float: 'right',
+                width: 240,
+                maxWidth: '25%',
+                marginTop: 0,
+                marginLeft: '1.375em',
+              }}
+              src="/speaking.jpg"
+              alt="Manuel Wieser speaking at Stahlstadt.js #18"
+            />
+            <p>
+              Do you want me to talk about web development topics at your event?
+              <br />
+              Send a message to{' '}
+              <a href="mailto:office@manuelwieser.com">
+                office@manuelwieser.com
+              </a>
+              !
+            </p>
+            <p>
+              <small>
+                Click on a talk’s title to download the slides. If there’s a
+                video recording it’s linked in the deck’s description.
+              </small>
+            </p>
+          </div>
+        </div>
+        <div className="Talks">
+          {projects.map(({ node: talk }) => {
+            return (
+              <div className="Talk">
+                <p className="Talk-meta">
+                  {talk.formattedDate} – {talk.location}
+                </p>
+                <h2 className="Talk-title">
+                  <a className="Talk-link" href={talk.slides}>
+                    {talk.title}
+                  </a>
+                </h2>
+                <p className="Talk-event">{talk.event}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </Layout>
   )
@@ -35,6 +72,7 @@ export const query = graphql`
           slides
           video
           date
+          formattedDate: date(formatString: "MMM DD, YYYY")
           location
         }
       }
