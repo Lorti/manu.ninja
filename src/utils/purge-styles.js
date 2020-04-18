211411045bfa66aae221fac28532433fc0c6efe5
Manuel Wieser <office@manuelwieser.com>
2020-04-18 19:38:41 +0200

    Add 11ty blog post

--- src/utils/purge-styles.js
+++ src/utils/purge-styles.js
@@ -1,12 +1,12 @@
 const generateCachedStylesheet = require('./build-styles');
-const { PurgeCSS: PurgeStyles } = require('purgecss');
+const { PurgeCSS } = require('purgecss');
 
 const styles = generateCachedStylesheet();
 const pattern = /<style>.*?<\/style>/s;
 
 module.exports = async function purgeStyles(content, path) {
   if (path.endsWith('.html')) {
-    const [{ css: result }] = await new PurgeStyles().purge({
+    const [{ css: result }] = await new PurgeCSS().purge({
       content: [{ raw: content.replace(pattern, ''), extension: 'html' }],
       css: [{ raw: styles }],
     });
